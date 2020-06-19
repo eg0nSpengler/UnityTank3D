1ccb837556584f80115b063a437b7834b4f7ba0f
eg0nSpengler <anthonyimanibrown@protonmail.com>
2020-06-19 18:18:04 -0400

    AI Pathing/Navigation Update, small audio addition.
    Removed NavMeshAgent related code in DetectionSphere and placed into entirely seperate script (NavComponent)
    
    Added related delegates/events to NavComponent and DetectionSphere
    
    Added Hovertank Tank Damage sound

--- Assets/Scripts/AI/DetectionSphere.cs
+++ Assets/Scripts/AI/DetectionSphere.cs
@@ -5,23 +5,60 @@ using UnityEngine.AI;
 
 public class DetectionSphere : MonoBehaviour
 {
- 
+
     [Header("References")]
     public GameObject currentTarget;
-    public Collider currentTargetCollider;
     public Collider audioTargetCollider;
 
-    private Vector3 _lastKnownPos;
     private SphereCollider _sphereCollider;
-    private NavMeshAgent _navAgent;
-    private NavMeshPath _navMeshPath;
 
-    private float _pathElapsed = 0.0f; //For NavMeshPath debugging purposes
     private float _sphereRadius = 5.0f;
+
+    public delegate void TargetClearLOS(GameObject obj);
+    public delegate void TargetNoClearLOS(GameObject obj);
+    public delegate void TargetInRadius(GameObject obj);
+    public delegate void TargetExitRadius(GameObject obj);
+    public delegate void TargetTracking(GameObject obj);
+
+    public delegate void HeardSound(GameObject obj);
+
+    /// <summary>
+    /// Called when we have a clear Line of Sight to a target
+    /// </summary>
+    public event TargetClearLOS OnTargetClearLOS;
+
+    /// <summary>
+    /// Called when we don't have a clear Line of Sight to a target
+    /// </summary>
+    public event TargetNoClearLOS OnTargetNoClearLOS;
+
+    /// <summary>
+    /// Called when a target has come inside the radius of our sphere
+    /// </summary>
+    public event TargetInRadius OnTargetInRadius;
+
+    /// <summary>
+    /// Called when a target exits the radius of our sphere
+    /// </summary>
+    public event TargetExitRadius OnTargetExitRadius;
+
+    /// <summary>
+    /// Called when we have a clear Line of Sight to a target and have begun tracking them
+    /// </summary>
+    public event TargetTracking OnTargetTracking;
+
+
+    /// <summary>
+    /// Called when we detect a noise
+    /// </summary>
+    public event HeardSound OnHeardSound;
 
     private void Awake()
     {
         _sphereCollider = GetComponent<SphereCollider>();
+
+        _sphereCollider.radius = _sphereRadius;
+        _sphereCollider.isTrigger = true;
 
         if (!_sphereCollider)
         {
@@ -29,98 +66,77 @@ public class DetectionSphere : MonoBehaviour
             _sphereCollider = gameObject.AddComponent<SphereCollider>();
         }
 
-        _navAgent = GetComponentInParent<NavMeshAgent>();
 
-        if (!_navAgent)
-        {
-            Debug.LogError("Failed to get NavMeshAgent on " + gameObject.name.ToString() + ", creating one now...");
-            _navAgent = gameObject.AddComponent<NavMeshAgent>();
-        }
-
-        _sphereCollider.radius = _sphereRadius;
-        _sphereCollider.isTrigger = true;
-        _navMeshPath = new NavMeshPath();
+        OnTargetTracking += SearchForTarget;
     }
 
     // Start is called before the first frame update
     private void Start()
     {
+        OnTargetTracking -= SearchForTarget;
+    }
 
+    private void OnDisable()
+    {
+        
     }
 
     // Update is called once per frame
     private void Update()
     {
-        //For drawing the NavMeshPath
-        _pathElapsed += Time.deltaTime;
-
-        if (_pathElapsed > 1.0f)
-        {
-            _pathElapsed -= 1.0f;
-            NavMesh.CalculatePath(gameObject.transform.position, _lastKnownPos, NavMesh.AllAreas, _navMeshPath);
-        }
-
-        for (var i = 0; i < _navMeshPath.corners.Length - 1; i++)
-        {
-            Debug.DrawLine(_navMeshPath.corners[i], _navMeshPath.corners[i + 1], Color.red);
-        }
-
-        SearchForTarget();
-        
-    }
-
-    private void FixedUpdate()
-    {
-        
+        SearchForTarget(currentTarget);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+
         // We detect a pickup in our radius
-        if(other.gameObject.tag == "Pickup")
+        if (other.gameObject.tag == TagStatics.GetPickupTag())
         {
-            if (InLineOfSight(other))
+            if (InLineOfSight(other.gameObject))
             {
-                MoveToTarget(other);
-            }
-        }
-        
-        if (other.gameObject.tag == "Mob" && other.gameObject.name == "TankActor")
-        {
-            //The mob is within our LoS
-            if (InLineOfSight(other))
-            {
-                //They're within our LoS, let's move to them
-                MoveToTarget(other);
+                OnTargetClearLOS(other.gameObject);
+                OnTargetInRadius(other.gameObject);
             }
             else
             {
-                //We don't have clear LoS to the target
-                //Let's atleast keep a reference to it should they suddenly come within our LoS
-                SetPossibleTarget(other);
-                return;
+                OnTargetNoClearLOS(other.gameObject);
+                OnTargetInRadius(other.gameObject);
             }
         }
+
+        if (other.gameObject.tag == TagStatics.GetMobTag() && other.gameObject.name == TagStatics.GetPlayerName())
+        {
+            //The mob is within our LoS
+            if (InLineOfSight(other.gameObject))
+            {
+                OnTargetClearLOS(other.gameObject);
+                OnTargetInRadius(other.gameObject);
+            }
+            else
+            {
+                OnTargetInRadius(other.gameObject);
+                OnTargetNoClearLOS(other.gameObject);
+                return;
+            }
+            currentTarget = other.gameObject;
+        }
+
 
         //We heard a noise
         if (other.gameObject.tag == "Audible")
         {
-            //Move to the source of said noise
-            MoveToAudible(other);
+            OnHeardSound(other.gameObject);
         }
 
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.gameObject.tag == "Mob" && other.gameObject.name == "TankActor")
+        if (other.gameObject.tag == TagStatics.GetMobTag() && other.gameObject.name == TagStatics.GetPlayerName())
         {
-            //They're out of our search radius, ignore them
-            Debug.Log(other.gameObject.ToString() + " has exited my search radius!");
+            OnTargetExitRadius(other.gameObject);
             currentTarget = null;
-            currentTargetCollider = null;
-            audioTargetCollider = null;
         }
     }
 
@@ -128,99 +144,48 @@ public class DetectionSphere : MonoBehaviour
     /// This is to check if we have an unobstructed LoS to whoever enters the monster's search radius
     /// If it's unobstructed, we know that we can "see" them
     /// </summary>
-    private bool InLineOfSight(Collider col)
+    private bool InLineOfSight(GameObject obj)
     {
         var startPos = gameObject.transform.position;
-        var endPos = (col.transform.position - gameObject.transform.position);
+        var endPos = (obj.transform.position - gameObject.transform.position);
         RaycastHit hit;
-       
+
         if (Physics.Raycast(startPos, endPos, out hit) == true)
         {
-            if (hit.collider.gameObject.name == col.gameObject.name)
+            if (hit.collider.gameObject.name == obj.name)
             {
+                OnTargetClearLOS(obj);
+                OnTargetInRadius(obj);
                 Debug.DrawRay(startPos, hit.point - startPos, Color.green);
-                return true;        
+                return true;
             }
             else
             {
+                OnTargetNoClearLOS(obj);
+                OnTargetInRadius(obj);
                 return false;
             }
         }
+
         return false;
     }
 
     /// <summary>
     /// This is to search for targets within our radius
     /// </summary>
-    private void SearchForTarget()
+    void SearchForTarget(GameObject obj)
     {
         //We have a reference to someone inside of our radius
-        if (currentTargetCollider)
+        if (currentTarget)
         {
+
             //Checking to see if they are actually within our radius
-            if (_sphereCollider.bounds.Contains(currentTargetCollider.bounds.center))
+            if (_sphereCollider.bounds.Contains(currentTarget.GetComponent<Collider>().bounds.center))
             {
-                //Do we have a clear LoS to the target?
-                //If so, move to it.
-                if (InLineOfSight(currentTargetCollider))
-                {
-                    _navAgent.SetDestination(currentTargetCollider.transform.position);
-                    _lastKnownPos = currentTarget.transform.position;
-                }
-            }
-        }
-        
-    }
-
-    /// <summary>
-    /// Moves to our target
-    /// </summary>
-    /// <param name="other"></param>
-    private void MoveToTarget(Collider other)
-    {
-        _navAgent.SetDestination(other.transform.position);
-        currentTarget = other.gameObject;
-        currentTargetCollider = other.gameObject.GetComponent<Collider>();
-    }
-
-    /// <summary>
-    /// We have a target within our search radius
-    /// We just don't have a clear LoS to them at this moment
-    /// Let's keep checking to see if we get a clear LoS to them while they are within out radius
-    /// </summary>
-    /// <param name="other"></param>
-    private void SetPossibleTarget(Collider other)
-    {
-        currentTargetCollider = other.gameObject.GetComponent<Collider>();
-        currentTarget = other.gameObject;
-    }
-
-    /// <summary>
-    /// I heard a noise, let's move to the source of that noise.
-    /// </summary>
-    /// <param name="other"></param>
-    private void MoveToAudible(Collider other)
-    {
-        _navAgent.SetDestination(other.transform.position);
-        audioTargetCollider = other.gameObject.GetComponent<Collider>();
-        currentTarget = other.gameObject;
-        _lastKnownPos = other.gameObject.transform.position;
-    }
-
-    private void ListenForAudible()
-    {
-        if (audioTargetCollider)
-        {
-            if (_sphereCollider.bounds.Contains(audioTargetCollider.bounds.center))
-            {
-                _navAgent.SetDestination(audioTargetCollider.transform.position);
-               _lastKnownPos = audioTargetCollider.transform.position;    
+                OnTargetClearLOS(currentTarget);
+                OnTargetTracking(currentTarget);
             }
         }
     }
-
-    void AttackTarget(Collider other)
-    {
-       
-    }
+    
 }
---
+++ Assets/Scripts/AI/NavComponent.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavComponent : MonoBehaviour
+{
+    public delegate void DestinationBegin();
+    public delegate void DestinationSuccess();
+    public delegate void DestinationFail();
+
+    public delegate void NavToTarget(GameObject obj);
+
+    /// <summary>
+    /// Called when the AI begins navigating to it's destination
+    /// </summary>
+    public event DestinationBegin OnDestinationBegin;
+
+    /// <summary>
+    /// Called when the AI successfully reaches it's destination
+    /// </summary>
+    public event DestinationSuccess OnDestinationSuccess;
+
+    /// <summary>
+    /// Called when the AI fails to reach it's destination
+    /// </summary>
+    public event DestinationFail OnDestinationFail;
+
+    /// <summary>
+    /// Called when the AI navigates to a target
+    /// </summary>
+    public event NavToTarget OnNavToTarget;
+
+    private GameObject _currentTarget;
+    private Vector3 _lastKnownPos;
+    private DetectionSphere _detectionSphere;
+
+    private NavMeshAgent _navAgent;
+    private NavMeshPath _navMeshPath;
+
+    private float _pathElapsed; //For NavMeshPath debugging purposes
+
+    private void Awake()
+    {
+        _navAgent = GetComponentInParent<NavMeshAgent>();
+        _detectionSphere = GetComponent<DetectionSphere>();
+
+        _pathElapsed = 0.0f;
+        _navMeshPath = new NavMeshPath();
+        _lastKnownPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+        if (!_navAgent)
+        {
+            Debug.LogError("Failed to get NavMeshAgent on " + gameObject.name.ToString() + ", creating one now...");
+            _navAgent = gameObject.AddComponent<NavMeshAgent>();
+        }
+
+        if (!_detectionSphere)
+        {
+            Debug.LogError("Failed to get DetectionSphere on " + gameObject.name.ToString() + ", creating one now...");
+            _detectionSphere = gameObject.AddComponent<DetectionSphere>();
+        }
+
+        _detectionSphere.OnTargetClearLOS += MoveToTarget;
+        _detectionSphere.OnTargetNoClearLOS += SetPossibleTarget;
+        _detectionSphere.OnTargetInRadius += SetPossibleTarget;
+        _detectionSphere.OnTargetExitRadius += ForgetTarget;
+        _detectionSphere.OnTargetExitRadius += MoveToLastKnownPos;
+        _detectionSphere.OnHeardSound += MoveToAudible;
+        _detectionSphere.OnTargetTracking += MoveToTarget;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+
+        _detectionSphere.OnTargetClearLOS -= MoveToTarget;
+        _detectionSphere.OnTargetNoClearLOS -= SetPossibleTarget;
+        _detectionSphere.OnTargetInRadius -= SetPossibleTarget;
+        _detectionSphere.OnTargetExitRadius -= ForgetTarget;
+        _detectionSphere.OnTargetExitRadius -= MoveToLastKnownPos;
+        _detectionSphere.OnHeardSound -= MoveToAudible;
+        _detectionSphere.OnTargetTracking -= MoveToTarget;
+    }
+
+    private void Update()
+    {
+        //For drawing the NavMeshPath
+        _pathElapsed += Time.deltaTime;
+
+        if (_pathElapsed > 1.0f)
+        {
+            _pathElapsed -= 1.0f;
+            NavMesh.CalculatePath(gameObject.transform.position, _lastKnownPos, NavMesh.AllAreas, _navMeshPath);
+        }
+
+        for (var i = 0; i < _navMeshPath.corners.Length - 1; i++)
+        {
+            Debug.DrawLine(_navMeshPath.corners[i], _navMeshPath.corners[i + 1], Color.red);
+        }
+
+    }
+
+    /// <summary>
+    /// Moves to our target
+    /// </summary>
+    private void MoveToTarget(GameObject obj)
+    {
+        _currentTarget = obj;
+        _navAgent.SetDestination(obj.transform.position);
+        //OnDestinationBegin();
+    }
+
+    /// <summary>
+    /// We have a target within our search radius
+    /// We just don't have a clear LoS to them at this moment
+    /// Let's keep checking to see if we get a clear LoS to them while they are within out radius
+    /// </summary>
+    private void SetPossibleTarget(GameObject obj)
+    {
+        _currentTarget = obj;
+    }
+
+    private void ForgetTarget(GameObject obj)
+    {
+        _lastKnownPos = _currentTarget.transform.position;
+        _currentTarget = null;
+    }
+
+    private void MoveToLastKnownPos(GameObject obj)
+    {
+        _navAgent.SetDestination(_lastKnownPos);
+    }
+
+    /// <summary>
+    /// I heard a noise, let's move to the source of that noise.
+    /// </summary>
+    /// <param name="other"></param>
+    private void MoveToAudible(GameObject obj)
+    {
+        _navAgent.SetDestination(obj.transform.position);
+        _currentTarget = obj;
+        _lastKnownPos = obj.transform.position;
+        //OnDestinationBegin();
+    }
+
+    IEnumerator CheckDist()
+    {
+        Debug.Log("I've begun navigating to a destination...");
+
+        // I don't know why but
+        // As great as predicates are
+        // That little () => bit of syntax has always irked me
+        // Like I see stray character(s) in code and my eyes dart to it's immediately
+        // Oh wait a minute it's just a predicate, nah it's cool
+        // It'd be like if you went walking around sipping from brown glass bottle
+        // People would obv think you'd be chugging down a cold one
+        // But wait a minute what's this?
+        // They see the label and see that its a cold one alright...
+        // A cold bottle of non-alcoholic Apple Cider
+        // Yep
+
+        yield return new WaitWhile(() => _navAgent.remainingDistance > _navAgent.stoppingDistance);
+
+    }
+
+    /// <summary>
+    /// This is to keep checking the distance between us and the destination while we're navigating
+    /// </summary>
+    private void UpdateDestStatus()
+    {
+        StartCoroutine(CheckDist());
+    }
+
+}
