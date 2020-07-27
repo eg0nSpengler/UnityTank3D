6464d01dc3e50650f136ec99bba6842faa359dd6
eg0nSpengler <anthonyimanibrown@protonmail.com>
2020-07-27 01:41:52 -0400

    Main Menu update, new settings button and panel
    Added new Settings button to Menu, clicking on said button will display game controls (more to come later)
    
    Added menu button prefab
    Added SettingsMenuButton script for handling of Settings button OnClick event
    Added BackButton script for handling of Back button logic in Settings panel
    Added MainMenuPanel script for handling of child panel activation/deactivation
    Added SettingsPanel script for handling of Settings panel

---
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuPanel : MonoBehaviour
+{
+    [Header("Button References")]
+    public GameObject StartButton;
+    public GameObject PlayLevelButton;
+    public GameObject ExitGameButton;
+    public GameObject SettingsButton;
+
+    [Header("Panel References")]
+    public GameObject SettingsPanel;
+
+    private List<GameObject> _menuButtons;
+    private List<GameObject> _menuPanels;
+
+    private void Awake()
+    {
+        _menuButtons = new List<GameObject>();
+        _menuPanels = new List<GameObject>();
+
+        if (!StartButton || !PlayLevelButton || !ExitGameButton || !SettingsButton)
+        {
+            Debug.LogWarning("MainMenuPanel is missing a Button reference!");
+        }
+
+        if (!SettingsPanel)
+        {
+            Debug.LogWarning("MainMenuPanel is missing a Panel reference!");
+        }
+
+        _menuButtons.Add(StartButton);
+        _menuButtons.Add(PlayLevelButton);
+        _menuButtons.Add(ExitGameButton);
+        _menuButtons.Add(SettingsButton);
+
+        _menuPanels.Add(SettingsPanel);
+
+        SettingsMenuButton.OnSettingsButtonClick += DisableButtons;
+        BackButton.OnBackButtonClick += EnableButtons;
+    }
+
+    private void OnDisable()
+    {
+        SettingsMenuButton.OnSettingsButtonClick -= DisableButtons;
+        BackButton.OnBackButtonClick -= EnableButtons;
+    }
+
+    void DisableButtons()
+    {
+        // When the settings button is clicked
+        // We simply disable all the buttons on the main menu panel
+        // Stops them from being displayed and processing mouse events
+
+        foreach (var button in _menuButtons)
+        {
+            button.SetActive(false);
+        }
+
+        //This enables the Settings panel
+        EnablePanel();
+    }
+
+    void EnableButtons()
+    {
+        //This disables the Settings panel
+        DisablePanel();
+
+        //Settings panel is now disabled
+        //Let's re enable our buttons
+        //So we can display them and have them process mouse events again
+
+        foreach (var button in _menuButtons)
+        {
+            button.SetActive(true);
+        }
+    }
+
+    void EnablePanel()
+    {
+        foreach (var panel in _menuPanels)
+        {
+            panel.SetActive(true);
+        }
+    }
+
+    void DisablePanel()
+    {
+        foreach (var panel in _menuPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+}
--- Assets/Scripts/UI/PlayLevelButton.cs
+++ Assets/Scripts/UI/PlayLevelButton.cs
@@ -2,22 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
-public class PlayLevelButton : MonoBehaviour
+public class PlayLevelButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Button _playLevelButton;
     private TMP_InputField _input;
+    private Color _defaultColor;
 
     private int _charLimit;
     private static string _savedNum;
 
     public delegate void PlayLevel();
+    public delegate void LevelNumProvided();
 
     /// <summary>
     /// Called when the user clicks on the Play Level button
     /// </summary>
     public static event PlayLevel OnPlayLevel;
+
+    /// <summary>
+    /// Called when there is valid text within the level number box
+    /// </summary>
+    public static event LevelNumProvided OnLevelNumProvided;
 
     private void Awake()
     {
@@ -25,6 +33,7 @@ public class PlayLevelButton : MonoBehaviour
         _input = GetComponentInChildren<TMP_InputField>();
         _charLimit = 2;
         _savedNum = "";
+        _defaultColor = _playLevelButton.image.color;
 
 
         if (!_playLevelButton)
@@ -40,6 +49,12 @@ public class PlayLevelButton : MonoBehaviour
         OnPlayLevel += ClearInput;
     }
 
+    private void OnEnable()
+    {
+        StartCoroutine(CheckTextForChar());
+        StartCoroutine(CheckTextLength());
+        ClearInput();
+    }
 
     private void OnDisable()
     {
@@ -50,15 +65,12 @@ public class PlayLevelButton : MonoBehaviour
     private void Start()
     {
         _playLevelButton.onClick.AddListener(HandleMouse);
-        StartCoroutine(CheckTextForChar());
-        StartCoroutine(CheckTextLength());
     }
 
-    private void Update()
-    {
-
-    }
-
+    /// <summary>
+    /// Checks the input field for characters and resets the field if any are detected
+    /// </summary>
+    /// <returns></returns>
     IEnumerator CheckTextForChar()
     {
         yield return new WaitWhile(() => _input.text.Length <= 0);
@@ -69,6 +81,7 @@ public class PlayLevelButton : MonoBehaviour
             {
                 Debug.LogWarning("Character detected!");
                 Debug.LogWarning("Please enter a number...");
+                StartCoroutine(FlashBox());
                 _input.text = "";
             }
         }
@@ -78,12 +91,17 @@ public class PlayLevelButton : MonoBehaviour
         StartCoroutine(CheckTextForChar());
     }
 
+    /// <summary>
+    /// Checks the length of the input field to see if it is >= 2 characters
+    /// </summary>
+    /// <returns></returns>
     IEnumerator CheckTextLength()
     {
         // Level numbers can be 2 digits MAX
         yield return new WaitWhile(() => _input.text.Length <= _charLimit);
         _input.text = "";
         Debug.LogWarning("Char limit reached!");
+        StartCoroutine(FlashBox());
         StartCoroutine(CheckTextLength());
     }
 
@@ -92,12 +110,16 @@ public class PlayLevelButton : MonoBehaviour
         OnPlayLevel?.Invoke();
     }
 
+    /// <summary>
+    /// Clears the input field
+    /// </summary>
     private void ClearInput()
     {
         // I know there is a builtin string method to remove chars from a string
         // But using that here forces the user to backspace/click the input box to enter text again
         // This makes it to whereas they can just immediately provide new input
         _input.text = "";
+        _input.image.color = _defaultColor;
     }
 
     /// <summary>
@@ -106,9 +128,39 @@ public class PlayLevelButton : MonoBehaviour
     /// <returns></returns>
     public static int GetNum()
     {
-        var numConv = _savedNum;
 
-        //Parses the string for integers and returns an int accordingly
-        return int.Parse(numConv, System.Globalization.NumberStyles.Integer);
+        if (_savedNum.Length > 0)
+        {
+            var numConv = _savedNum;
+
+            //Parses the string for integers and returns an int accordingly
+            return int.Parse(numConv, System.Globalization.NumberStyles.Integer);
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _playLevelButton.image.color = Color.green;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _playLevelButton.image.color = _defaultColor;
+    }
+
+    /// <summary>
+    /// Changes the input box color to Red for half a second
+    /// This is done to communicate to the user that they've provided invalid text to the input field
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator FlashBox()
+    {
+        _input.image.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+        _input.image.color = _defaultColor;
     }
 }
