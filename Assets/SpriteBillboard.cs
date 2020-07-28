56b27b63ae391b1f5f89d042a3bf223d52f0e857
eg0nSpengler <anthonyimanibrown@protonmail.com>
2020-07-28 16:27:28 -0400

    Small Sprite QoL addition
    Added SpriteBillboard script to make it to whereas sprites will always face forward towards the player camera

---
+++ Assets/SpriteBillboard.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteBillboard : MonoBehaviour
+{
+    /// <summary>
+    /// thanks 10yr+ old Unity thread
+    /// </summary>
+    // Update is called once per frame
+    void Update()
+    {
+        // This just makes it to whereas the sprites always face forward towards the player camera
+        gameObject.GetComponent<Transform>().LookAt(Camera.main.transform.position, Vector3.up);
+    }
+}
